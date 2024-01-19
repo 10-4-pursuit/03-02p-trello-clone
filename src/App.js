a6c9e084aba8ecc48dce3640b763ee946f2b64ca
GyancarlosPinto <gyancarlospinto@pursuit.org>
2024-01-19 15:20:59 -0500

    Initializing Phase 1 Build of Project

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        
+        <h1>Vision Board</h1>
       </header>
+      <body>
+        
+      </body>
     </div>
   );
 }
