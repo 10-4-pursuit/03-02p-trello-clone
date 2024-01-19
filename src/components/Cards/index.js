31f6db8904b7e088d4a92f9a77d36ba6124fd57c
GyancarlosPinto <gyancarlospinto@pursuit.org>
2024-01-19 15:19:10 -0500

    Building out first form version for Cards Comp

--- src/components/Cards/index.js
+++ src/components/Cards/index.js
@@ -2,4 +2,29 @@ import React from "react";
 
 export default function Cards() {
     
+    return (
+        <div>
+            <form>
+                <input 
+                type="textarea"
+                placeholder="Title"
+                />
+                <input
+                type="textarea"
+                placeholder="Description"
+                />
+                <input
+                type="textarea"
+                placeholder="Comments"
+                />
+                <input
+                type="date"
+                />
+                <input 
+
+                />
+            </form>
+
+        </div>
+    )
 }
