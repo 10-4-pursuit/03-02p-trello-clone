5236bd313898b33c248c94be450b259f55006695
GyancarlosPinto <gyancarlospinto@pursuit.org>
2024-01-22 10:03:40 -0500

    Initialized Rendering elements, values and Comps inLists

--- src/components/Lists/index.js
+++ src/components/Lists/index.js
@@ -1,10 +1,14 @@
 import React from "react";
+import Cards from "../Cards";
 
-export default function Lists() {
+export default function Lists({ title, cards }) {
     
     return (
-        <div>
-            
+        <div className="list">
+            <h3>{title}</h3>
+            {cards.map(card => (
+                <Cards key={card.id} {...card} />
+            ))}
         </div>
     )
 }
