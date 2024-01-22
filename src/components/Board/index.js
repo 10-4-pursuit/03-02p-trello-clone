2d43cbcb6a9956622e64f20be805c299293e9586
GyancarlosPinto <gyancarlospinto@pursuit.org>
2024-01-22 09:48:23 -0500

    Updating Board to Render List Comp

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,10 +1,13 @@
 import React from "react";
+import Lists from "../Lists";
 
-export default function Board() {
+export default function Board({ lists }) {
     
     return (
         <div className="board">
-            
+            {lists.map(list => (
+                <Lists key={list.id} title={list.title} cards={list.cards} />
+            ))}
         </div>
     )
 }
