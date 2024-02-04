5b57bbc43b0cf8338b00bb997c7cd37fdd688dec
GyancarlosPinto <gyancarlospinto@pursuit.org>
2024-02-04 15:50:41 -0500

    Adding listId key value to render

--- src/components/Board/index.js
+++ src/components/Board/index.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { Children } from "react";
 import Lists from "../Lists";
 
 export default function Board({ lists }) {
@@ -6,8 +6,9 @@ export default function Board({ lists }) {
     return (
         <div className="board">
             {lists.map(list => (
-                <Lists key={list.id} title={list.title} cards={list.cards} />
+                <Lists key={list.id} listId={list.id} title={list.title} cards={list.cards} />
             ))}
+            
         </div>
     )
 }
