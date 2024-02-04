b879637af92b4f5662b337d1b6c23129fb5ea045
GyancarlosPinto <gyancarlospinto@pursuit.org>
2024-02-04 15:55:17 -0500

    Updates to App.js/css files and index.js file

--- src/App.js
+++ src/App.js
@@ -3,20 +3,10 @@ import React from 'react';
 import Board from './components/Board';
 import Lists from './components/Lists';
 import Cards from './components/Cards';
+import BoardContext from './helpers/BoardContext';
 
 function App() {
-
-  const [board, setBoard] = React.useState({
-    lists: [
-      {
-        id: 1,
-        title: 'To Do',
-        cards: [
-          { id: 1, title: 'Task 1', description: 'Description 1', comments: [], dueDate: '2022-09-01', labels: [] },
-        ]
-      },
-    ]
-  });
+  const { board, setBoard, showNewListForm,setShowNewListForm, newListTitle, setNewListTitle, handleCardSubmit, handleListSubmit } = React.useContext(BoardContext)
 
   React.useEffect(() => {
     const savedBoard = localStorage.getItem('visionBoard');
@@ -30,54 +20,14 @@ function App() {
     localStorage.setItem('visionBoard', JSON.stringify(board));
   }, [board]);
 
-  const [showNewListForm, setShowNewListForm] = React.useState(false);
-
-  const [newListTitle, setNewListTitle] = React.useState('');
-
-  const handleCardSubmit = (listId, cardData) => {
-    const newBoard = { ...board };
-    const listIndex = newBoard.lists.findIndex(list => list.id === listId);
-
-    if (listIndex > -1) {
-      newBoard.lists[listIndex].cards.push({
-        id: Date.now(),
-        ...cardData
-      });
-      setBoard(newBoard);
-    }
-  };
-
-  const handleListSubmit = (event) => {
-    event.preventDefault();
-    if (newListTitle.trim() !== "") {
-      const newList = {
-        id: Date.now(),
-        title: newListTitle,
-        cards: []
-      };
-      setBoard({ ...board, lists: [...board.lists, newList] });
-      setNewListTitle('');
-      setShowNewListForm(false);
-    }
-  };
-
   return (
     <div className="App">
-      <h1>Vision Board</h1>
       <header className="App-header">
-
+      <h1>Vision Board</h1>
       </header>
       <main className='App-content'>
-        <Board lists={board.lists}>
-          {board.lists.map(list => (
-            <Lists key={list.id} title={list.title} initialCards={list.cards}>
-              {list.cards.map(card => (
-                <Cards key={card.id} {...card} onCardSubmit={(cardData) => handleCardSubmit(list.id, cardData)} />
-              ))}
-            </Lists>
-          ))}
-
-          {showNewListForm ? (
+        <Board lists={board.lists} />
+        {showNewListForm ? (
             <form onSubmit={handleListSubmit}>
               <input
                 type='text'
@@ -89,9 +39,6 @@ function App() {
           ) : (
             <button onClick={() => setShowNewListForm(true)}>Add New List</button>
           )}
-
-        </Board>
-
       </main>
 
     </div>
